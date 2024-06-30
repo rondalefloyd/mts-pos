c62013b9b0dffcb1a1ec2860242e3333bc428b30
Rondale Floyd Bufete <bufete.rondalefloyd@gmail.com>
2024-06-30 19:31:20 +0800

    fix uis

--- app/ui/dialogs/edit_current_userui_ui.py
+++ app/ui/dialogs/edit_current_userui_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\Users\mimoy\Documents\GitHub\mts-pos\app\ui\dialogs\user_config.ui'
+# Form implementation generated from reading ui file 'c:\Users\mimoy\Documents\GitHub\mts-pos\app\ui\dialogs\edit_current_userui.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.10
 #
