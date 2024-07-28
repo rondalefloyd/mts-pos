d76cca13a19313c48ca821df9813e1a19d00bcdd
Rondale Floyd Bufete <bufete.rondalefloyd@gmail.com>
2024-07-28 16:04:39 +0800

    updated

--- app/controllers/common/messages.py
+++ app/controllers/common/messages.py
@@ -1,24 +1,9 @@
 import re
 
-def function_route_error_message(function_route, error):
-    message = (
-        f"An error occured in <b>{function_route}</b>.<br>"
-        f"Error: <i>{error}</i><br>"
-        f"Please contact customer support."
-    )
-    
-    return message
-
-def class_error_message(class_name):
-    message = f"Something went wrong in {class_name}"
-    
-    return message
-
 def function_route_not_exist(function_route, class_name):
     message =  f"<b>{function_route}</b> does not exist. Please check <b>{class_name}</b>."
     
     return message
-
 
 def exception_error_message(error):
     return f'Something went wrong: {error}'
@@ -28,4 +13,4 @@ def integrity_error_message(error):
     
     if match:
         field_name = match.group(1).replace('"', '')
-        return f"{field_name} already exists."
+        return f"<b>{field_name}</b> already exists."
