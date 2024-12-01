1e5477c52e7fa5104f290596484ec23936525e9e
unknown <bufete.rondalefloyd@gmail.com>
2024-12-01 11:56:27 +0800

    updated

--- start.bat
+++ start.bat
@@ -5,5 +5,5 @@ call .\venv\Scripts\activate
 echo Running the Python application...
 python app/main.py >> logs.txt 2>&1
 
-echo Application has finished running. Check logs.txt for output.
+echo Application has finished running.
 pause
